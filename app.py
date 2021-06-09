1120938fd262ee840b4184da5d81319c8e57c579
Pooja B <53658738+poojabasker@users.noreply.github.com>
2021-06-09 20:28:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,18 @@ from keras.models import model_from_json,model_from_yaml
 import clean_data as cd
 import json
 import requests
-
 yaml_file = open('model.yaml', 'r')
 loaded_model_yaml = yaml_file.read()
 yaml_file.close()
 model = model_from_yaml(loaded_model_yaml)
 # load weights into new model
 model.load_weights("model.h5")
-
 # app
 app = Flask(__name__)
-
 # routes
 @app.route('/', methods=['GET'])
 def home():
     return ("Hello world!")
-
 @app.route('/predict', methods=['POST'])
 def predict():
     # get data
@@ -33,13 +29,11 @@ def predict():
     #result = json.dumps(result)
 
     # send back to browser
-    output = result
     temp = sorted(result.items(), key=lambda item: item[1], reverse=True)
     result = dict(temp[0:3])
 
     # return data
     #return data
     return jsonify(result)
-
 if __name__ == '__main__':
    app.run(debug=true)
